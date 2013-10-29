1dcdabe32938004be95e0fb5d46eb0d4584fc8e9
Andrey Antukh <niwi@niwi.be>
2013-10-29 23:11:31 +0100

    Add nn_freemsg native method.

--- src/java/nanomsg/jna/NanoMsg.java
+++ src/java/nanomsg/jna/NanoMsg.java
@@ -13,6 +13,7 @@ public class NanoMsg {
     public static native String nn_strerror (int errnum);
     public static native int nn_setsockopt (int s, int level, int option, String optval, int optvallen);
     public static native Pointer nn_symbol(int i, IntByReference value);
+    public static native int nn_freemsg (Pointer msg);
 
     static {
         Native.register("nanomsg");
