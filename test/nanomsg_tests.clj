74e7f4bb5c6fe77d9a89da66420d8429c4949065
Andrey Antukh <niwi@niwi.be>
2013-10-30 20:56:22 +0100

    Add test to pair and bus sockets.

--- test/nanomsg_tests.clj
+++ test/nanomsg_tests.clj
@@ -6,17 +6,12 @@
 (def s-bind-ipc "ipc:///tmp/test.ipc")
 (def s-bind-inproc "inproc://test")
 
-(defn thread
-  [func]
-  (let [t (Thread. func)]
-    (.start t)))
-
 (defn sleep
   [t]
   (Thread/sleep t))
 
 (deftest main-tests
-  (testing "Simple send/reply"
+  (testing "ReqRep"
     (let [req-sock (nn/socket :req)
           result (future
                    (let [rep-sock (nn/socket :rep)]
@@ -24,11 +19,12 @@
                      (let [received (nn/recv rep-sock)]
                        (nn/send rep-sock received)
                        received)))]
+      (sleep 500)
       (nn/connect req-sock s-bind-ipc)
       (nn/send req-sock "foo")
       (is (= (nn/recv req-sock) "foo"))))
 
-  (testing "Simple subscribe"
+  (testing "PubSub"
     (let [pub-sock (nn/socket :pub)]
       (nn/bind pub-sock s-bind-ipc)
       (let [recvd1   (future
@@ -42,8 +38,37 @@
                          (nn/subscribe sub-socket "ss2")
                          (nn/recv sub-socket)))]
 
+        (sleep 500)
         (nn/send pub-sock "ss2 foo")
         (nn/send pub-sock "ss1 bar")
 
         (is (= @recvd1 "ss1 bar"))
-        (is (= @recvd2 "ss2 foo"))))))
+        (is (= @recvd2 "ss2 foo")))))
+
+  (testing "Pair"
+    (let [sock (nn/socket :pair)]
+      (nn/bind sock s-bind-tcp)
+      (let [recvd1   (future
+                       (let [socket (nn/socket :pair)]
+                         (nn/connect socket s-bind-tcp)
+                         (nn/recv socket)))]
+        (sleep 500)
+        (nn/send sock "foo")
+        (is (= @recvd1 "foo")))))
+
+  (testing "Bus"
+    (let [sock (nn/socket :bus)]
+      (nn/bind sock s-bind-ipc)
+      (let [recvd1   (future
+                       (let [socket (nn/socket :bus)]
+                         (nn/connect socket s-bind-ipc)
+                         (nn/recv socket)))
+            recvd2   (future
+                       (let [socket (nn/socket :bus)]
+                         (nn/connect socket s-bind-ipc)
+                         (nn/recv socket)))]
+
+        (sleep 500)
+        (nn/send sock "foo")
+        (is (= @recvd1 "foo")
+        (is (= @recvd2 "foo")))))))
