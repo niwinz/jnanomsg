8bee98fc32d9fff7bd556a3cb8bc4d95ca02c465
Andrey Antukh <niwi@niwi.be>
2013-10-30 19:15:28 +0100

    Add nonmblocking way to receive and send data with sockets.

--- project.clj
+++ project.clj
@@ -12,7 +12,7 @@
   :source-paths ["src/clojure"]
   :java-source-paths ["src/java"]
   :plugins [[lein-javadoc "0.1.1"]]
-  :javadoc-opts {:output-dir "doc/"
+  :javadoc-opts {:output-dir "docs/_build/javadoc"
                  :package-names ["nanomsg" "nanomsg.ffi" "nanomsg.pubsub"]}
   :main nanomsg.test
   :profiles {:uberjar {:aot :all}})
--- src/java/nanomsg/RWSocket.java
+++ src/java/nanomsg/RWSocket.java
@@ -11,35 +11,103 @@ public abstract class RWSocket extends Socket {
         super(domain, protocol);
     }
 
-    public void sendString(String data) {
+    /**
+     * Send string to socket with option to set blocking flag.
+     *
+     * @param blocking set blocking or non blocking flag.
+     * @return receved data as unicode string.
+     */
+    public void sendString(String data, boolean blocking) {
         Charset encoding = Charset.forName("UTF-8");
-        this.sendBytes(data.getBytes(encoding));
+        this.sendBytes(data.getBytes(encoding), blocking);
     }
 
-    public void sendBytes(byte[] data) {
-        int rc = NanoMsg.nn_send(this.socket, data, data.length, 0);
+    /**
+     * Send string to socket.
+     *
+     * This operation is blocking by default.
+     *
+     * @return receved data as unicode string.
+     */
+    public void sendString(String data) {
+        this.sendString(data, true);
+    }
+
+    /**
+     * Send byte array to socket with option to set blocking flag.
+     *
+     * @param blocking set blocking or non blocking flag.
+     * @return receved data as unicode string.
+     */
+    public void sendBytes(byte[] data, boolean blocking) {
+        int rc = NanoMsg.nn_send(this.socket, data, data.length, blocking ? Constants.NN_DONTWAIT : 0);
         if (rc < 0) {
             System.out.println("Error");
         }
     }
 
-    public String recvString() throws RuntimeException {
-        byte[] received = this.recvBytes();
+    /**
+     * Send byte array to socket.
+     *
+     * This operation is blocking by default.
+     *
+     * @return receved data as unicode string.
+     */
+    public void sendBytes(byte[] data) {
+        this.sendBytes(data, true);
+    }
+
+    /**
+     * Receive string from socket with option to set blocking flag.
+     *
+     * @param blocking set blocking or non blocking flag.
+     * @return receved data as unicode string.
+     */
+    public String recvString(boolean blocking) throws RuntimeException {
+        byte[] received = this.recvBytes(blocking);
         Charset encoding = Charset.forName("UTF-8");
 
         return new String(received, encoding);
     }
 
-    public byte[] recvBytes() throws RuntimeException {
+    /**
+     * Receive string from socket.
+     *
+     * This operation is blocking by default.
+     *
+     * @return receved data as unicode string.
+     */
+    public String recvString() throws RuntimeException {
+        return this.recvString(true);
+    }
+
+    /**
+     * Receive byte array from socket with option to set blocking flag.
+     *
+     * @param blocking set blocking or non blocking flag.
+     * @return receved data as unicode string.
+     */
+    public byte[] recvBytes(boolean blocking) throws RuntimeException {
         Pointer buff = Pointer.NULL;
         PointerByReference ptrBuff = new PointerByReference(buff);
 
-        int received = NanoMsg.nn_recv(this.socket, ptrBuff, Constants.NN_MSG, 0);
+        int received = NanoMsg.nn_recv(this.socket, ptrBuff, Constants.NN_MSG, blocking ? Constants.NN_DONTWAIT : 0);
         if (received < 0) {
             throw new RuntimeException("error on rcv");
         }
 
         Pointer result = ptrBuff.getValue();
         return result.getByteArray(0, received);
+    }
+
+    /**
+     * Receive byte array from socket.
+     *
+     * This operation is blocking by default.
+     *
+     * @return receved data as unicode string.
+     */
+    public byte[] recvBytes() throws RuntimeException {
+        return this.recvBytes(true);
     }
 }
