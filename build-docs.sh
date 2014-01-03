32d32746535f86e40f3a38824c4c754da26f552e
Andrey Antukh <niwi@niwi.be>
2014-01-03 16:15:01 +0100

    Update build-docs script.

--- build-docs.sh
+++ build-docs.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 (cd docs; make)
 cp -vr docs/index.html /tmp/index.html;
+cp -vr docs/api /tmp/api
 git checkout gh-pages;
 rm -rf *
 mv -fv /tmp/index.html .
-git add .
+mv -fv /tmp/api .
+
+git add --all index.html
+git add --all api
 git commit -a -m "Update doc"
