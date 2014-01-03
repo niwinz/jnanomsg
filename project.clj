f040e5c6057dbc6f706f0f2105e991be43bb7547
Andrey Antukh <niwi@niwi.be>
2014-01-03 14:47:13 +0100

    Add core.async dependency.

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Apache 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.txt"}
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
                  [net.java.dev.jna/jna "4.0.0"]]
   :jvm-opts ["-server" "-Xmx2g" "-XX:+UseG1GC"]
   :source-paths ["src/clojure"]
