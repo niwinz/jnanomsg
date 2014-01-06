d579aba62459299bcabeb323f9a61495ac68a354
Andrey Antukh <niwi@niwi.be>
2014-01-06 18:44:32 +0100

    Add device support to java bindings.

---
+++ src/java/nanomsg/Device.java
@@ -0,0 +1,19 @@
+package nanomsg;
+
+import java.lang.Runnable;
+import nanomsg.NativeLibrary;
+
+
+public class Device implements Runnable {
+    private final int sourceSocket;
+    private final int destSocket;
+
+    public Device(final int sourceSocket, final int destSocket) {
+        this.sourceSocket = sourceSocket;
+        this.destSocket = destSocket;
+    }
+
+    public void run() {
+        NativeLibrary.nn_device(sourceSocket, destSocket);
+    }
+}
