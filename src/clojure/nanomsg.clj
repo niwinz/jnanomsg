623390047368fbce1fd313b0f947c4ef6c9a82f0
Andrey Antukh <niwi@niwi.be>
2014-01-12 21:00:39 +0100

    Now all code sync and async are implemented using protocols.

--- src/clojure/nanomsg.clj
+++ src/clojure/nanomsg.clj
@@ -17,31 +17,103 @@
            (nanomsg.pipeline PushSocket PullSocket)
            (nanomsg.pair PairSocket)
            (nanomsg.bus BusSocket)
-           (nanomsg Socket Nanomsg Device)))
+           (nanomsg ISocket Socket Nanomsg Device)
+           (nanomsg.async IAsyncCallback AsyncSocketProxy))
+  (:require [clojure.core.async :refer [chan put!]]))
 
 (def ^:static ^:private supported-sockets
-  {:pub PubSocket
-   :sub SubSocket
-   :req ReqSocket
-   :rep RepSocket
-   :bus BusSocket
-   :pair PairSocket
-   :push PushSocket
-   :pull PullSocket})
+  {:pub PubSocket   :sub SubSocket
+   :req ReqSocket   :rep RepSocket
+   :bus BusSocket   :pair PairSocket
+   :push PushSocket :pull PullSocket})
 
-(defn bind!
-  "Given a socket and connection string,
-  add a local endpoint to the socket."
-  [^Socket sock, ^String dir]
-  (.bind sock dir)
-  sock)
+(defn- make-iasynccallback
+  "Private function that create a new
+  instance of IAsyncCallback."
+  [ch]
+  (reify IAsyncCallback
+    (success [_ result]
+      (put! ch result))
+    (fail [_ throwable]
+      (put! ch throwable))))
 
-(defn connect!
-  "Given a socket and connection string,
-  connect to remote socket."
-  [^Socket sock, ^String dir]
-  (.connect sock dir)
-  sock)
+(defprotocol INNSocket
+  (bind! [_ dir] "Bind to endpoint specified by dir parameter.")
+  (connect! [_ dir] "Connect to endpoint specified by dir parameter.")
+  (subscribe! [_ pattern] "Subscribe current socket to specified pattern.")
+  (send! [_ data] [_ data blocking] "Send string using a socket.")
+  (recv! [_] [_ blocking] "Receive string using a socket.")
+  (send-bytes! [_ data] [_ data blocking] "Send bytes using socket.")
+  (recv-bytes! [_] [_ blocking] "Receive bytes using socket.")
+  (close! [_] "Close a socket."))
+
+(defrecord Connection [s]
+  INNSocket
+  (bind! [_ endpoint]
+    (.bind s endpoint))
+  (connect! [_ endpoint]
+    (.connect s endpoint))
+  (subscribe! [_ pattern]
+    (.subscribe s pattern))
+  (send-bytes! [_ data]
+    (.sendBytes s data))
+  (recv-bytes! [_]
+    (.recvBytes s))
+  (send-bytes! [_ data blocking]
+    (.sendBytes s data blocking))
+  (recv-bytes! [_ blocking]
+    (.recvBytes s blocking))
+  (send! [_ data]
+    (.sendString s data))
+  (recv! [_]
+    (.recvString s))
+  (send! [_ data blocking]
+    (.sendString s data blocking))
+  (recv! [_ blocking]
+    (.recvString s blocking))
+  (close! [_]
+    (.close s)))
+
+(defrecord AsyncConnection [s px]
+  INNSocket
+
+  (bind! [_ endpoint]
+    (.bind s endpoint))
+  (connect! [_ endpoint]
+    (.connect s endpoint))
+  (subscribe! [_ pattern]
+    (.subscribe s pattern))
+  (send-bytes! [_ data]
+    (let [ch (chan)
+          cb (make-iasynccallback ch)]
+      (.sendBytes px cb)
+      ch))
+  (recv-bytes! [_]
+    (let [ch (chan)
+          cb (make-iasynccallback ch)]
+      (.recvBytes px cb)
+      ch))
+  (send! [_ data]
+    (let [ch (chan)
+          cb (make-iasynccallback ch)]
+      (.sendString px data cb)
+      ch))
+  (recv! [_]
+    (let [ch (chan)
+          cb (make-iasynccallback ch)]
+      (.recvString px cb)
+      ch))
+  (close! [_]
+    (.close s))
+  (send! [_ data blocking]
+    (throw (UnsupportedOperationException. "Unsuporded arity for async connection.")))
+  (recv! [_ blocking]
+    (throw (UnsupportedOperationException. "Unsuporded arity for async connection.")))
+  (send-bytes! [_ data blocking]
+    (throw (UnsupportedOperationException. "Unsuporded arity for async connection.")))
+  (recv-bytes! [_ blocking]
+    (throw (UnsupportedOperationException. "Unsuporded arity for async connection.")))
+  )
 
 (defn socket
   "Geiven a socket type, create a new instance
@@ -49,68 +121,15 @@
   ([^clojure.lang.Keyword socktype] (socket socktype {}))
   ([^clojure.lang.Keyword socktype opts]
    {:pre [(supported-sockets socktype)]}
-   (let [cls      (-> socktype supported-sockets)
-         instance (.newInstance cls)]
+   (let [cls        (-> socktype supported-sockets)
+         instance   (.newInstance cls)
+         conn       (if (:async opts)
+                      (->AsyncConnection instance (AsyncSocketProxy. instance))
+                      (->Connection instance))]
      (cond
-       (:bind opts) (bind! instance (:bind opts))
-       (:connect opts) (connect! instance (:connect opts)))
-     instance)))
-
-(defn subscribe!
-  "Subscribe a current subscriber socket
-  to specified string pattern."
-  [^SubSocket sock, ^String pattern]
-  {:pre [(instance? SubSocket sock)]}
-  (.subscribe sock pattern))
-
-(defn send-bytes!
-  "Given a socket and message as bytes array,
-  send it throught the socket.
-
-  NOTE: This function is a low level interface and
-  theorically does not has overhead over
-  raw socket usage."
-  [^Socket sock, ^bytes msg & {:keys [blocking] :or {blocking true}}]
-  (.sendBytes sock msg blocking))
-
-(defn recv-bytes!
-  "Given a socket, receive a message as byte array.
-
-  This function, uses a low level access for
-  receiving a message, that removes a overhead
-  of creating a message object."
-  [^Socket sock & {:keys [blocking] :or {blocking true}}]
-  (.recvBytes sock blocking))
-
-(defn send!
-  "Given a socket and message as string, send
-  it throught the socket.
-
-  This function uses a low level method for sending
-  messages that removes a overhead of creating
-  nanomsg.Message objects.
-
-  utf-8 encoding is unique overhead that
-  has this function over using a raw socket."
-  [^Socket sock, ^String msg & {:keys [blocking] :or {blocking true}}]
-  (.sendString sock msg blocking))
-
-(defn recv!
-  "Given a socket, receive a message as string.
-
-  This function, uses a low level access for
-  receiving a message, that removes a overhead
-  of creating a message object.
-
-  utf-8 decoding is a unique overhead
-  that has this function over using a raw socket."
-  [^Socket sock & {:keys [blocking] :or {blocking true}}]
-  (.recvString sock blocking))
-
-(defn close!
-  "Close socket."
-  [^Socket sock]
-  (.close sock))
+       (:bind opts) (bind! conn (:bind opts))
+       (:connect opts) (connect! conn (:connect opts)))
+     conn)))
 
 (defn terminate!
   "Send terminate signal to all open and/or blocked
@@ -125,8 +144,7 @@
   (let [d (Device. s1 s2)]
     (.run d)))
 
-(defn- resolve-symbols
-  []
+(defn- resolve-symbols []
   (into {} (for [[k v] (Nanomsg/getSymbols)]
              [(keyword (.toLowerCase k)) v])))
 
