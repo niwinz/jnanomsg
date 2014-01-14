f04d5d21f71e36c4af1ada3296172744d2aeb7e4
Andrey Antukh <niwi@niwi.be>
2014-01-14 19:34:54 +0100

    Reordering fields on ISocket interface.

--- src/java/nanomsg/ISocket.java
+++ src/java/nanomsg/ISocket.java
@@ -11,11 +11,21 @@ public interface ISocket {
     public int getNativeSocket();
     public void bind(final String dir) throws IOException;
     public void connect(final String dir) throws IOException;
-
-    public int sendString(final String data, final boolean blocking) throws IOException, EAgainException;
-    public int sendBytes(final byte[] data, final boolean blocking) throws IOException, EAgainException;
-    public String recvString(final boolean blocking) throws IOException, EAgainException;
-    public byte[] recvBytes(final boolean blocking) throws IOException, EAgainException;
     public void subscribe(final String data) throws IOException;
+
+    public int sendString(final String data, final boolean blocking) throws IOException, IOException;
+    public int sendString(final String data) throws IOException, IOException;
+    public int sendBytes(final byte[] data, final boolean blocking) throws IOException, IOException;
+    public int sendBytes(final byte[] data) throws IOException, IOException;
+
+    public String recvString(final boolean blocking) throws IOException, IOException;
+    public String recvString() throws IOException, IOException;
+
+    public byte[] recvBytes(final boolean blocking) throws IOException, IOException;
+    public byte[] recvBytes() throws IOException, IOException;
+
     public int getFd(final int flag) throws IOException;
+
+    public void setSendTimeout(final int milis);
+    public void setRecvTimeout(final int milis);
 }
