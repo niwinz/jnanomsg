90fee5390562899a834442efa984726b69fcd6de
Andrey Antukh <niwi@niwi.be>
2015-02-11 20:26:07 +0100

    Fix code style on Device.java.

--- src/java/nanomsg/Device.java
+++ src/java/nanomsg/Device.java
@@ -5,15 +5,15 @@ import nanomsg.NativeLibrary;
 
 
 public class Device implements Runnable {
-    private final int sourceSocket;
-    private final int destSocket;
+  private final int sourceSocket;
+  private final int destSocket;
 
-    public Device(final int sourceSocket, final int destSocket) {
-        this.sourceSocket = sourceSocket;
-        this.destSocket = destSocket;
-    }
+  public Device(final int sourceSocket, final int destSocket) {
+    this.sourceSocket = sourceSocket;
+    this.destSocket = destSocket;
+  }
 
-    public void run() {
-        NativeLibrary.nn_device(sourceSocket, destSocket);
-    }
+  public void run() {
+    NativeLibrary.nn_device(sourceSocket, destSocket);
+  }
 }
