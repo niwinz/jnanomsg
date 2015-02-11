74603dbb3c077c5e81fa0919d158979370642023
Andrey Antukh <niwi@niwi.be>
2015-02-11 20:26:32 +0100

    Fix code style on Message.java.

--- src/java/nanomsg/Message.java
+++ src/java/nanomsg/Message.java
@@ -2,29 +2,29 @@ package nanomsg;
 import java.nio.charset.Charset;
 
 public class Message {
-    private final byte[] messageData;
+  private final byte[] messageData;
 
-    public Message(final String data) {
-        messageData = data.getBytes(Charset.forName("UTF-8"));
-    }
+  public Message(final String data) {
+    messageData = data.getBytes(Charset.forName("UTF-8"));
+  }
 
-    public Message(final byte[] data) {
-        messageData = data;
-    }
+  public Message(final byte[] data) {
+    messageData = data;
+  }
 
-    public String toString() {
-        return new String(messageData, Charset.forName("UTF-8"));
-    }
+  public String toString() {
+    return new String(messageData, Charset.forName("UTF-8"));
+  }
 
-    public String toString(String encoding) {
-        return new String(messageData, Charset.forName(encoding));
-    }
+  public String toString(String encoding) {
+    return new String(messageData, Charset.forName(encoding));
+  }
 
-    public String toString(Charset encoding) {
-        return new String(messageData, encoding);
-    }
+  public String toString(Charset encoding) {
+    return new String(messageData, encoding);
+  }
 
-    public byte[] toBytes() {
-        return messageData;
-    }
+  public byte[] toBytes() {
+    return messageData;
+  }
 }
