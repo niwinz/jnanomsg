caab260289272def7507fd858510e3b5217ed340
Andrey Antukh <niwi@niwi.be>
2015-02-11 20:28:40 +0100

    Fix code style on EAgainException.java.

--- src/java/nanomsg/exceptions/EAgainException.java
+++ src/java/nanomsg/exceptions/EAgainException.java
@@ -3,8 +3,8 @@ package nanomsg.exceptions;
 import nanomsg.exceptions.IOException;
 
 public class EAgainException extends IOException {
-    public EAgainException(IOException cause) {
-        super(cause);
-        this.errno = cause.getErrno();
-    }
+  public EAgainException(IOException cause) {
+    super(cause);
+    this.errno = cause.getErrno();
+  }
 }
