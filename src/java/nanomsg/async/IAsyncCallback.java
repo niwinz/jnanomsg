747c6dde2d39e78f007de0baafb013a66ed26ae3
Andrey Antukh <niwi@niwi.be>
2015-02-11 20:27:46 +0100

    Fix code style on IAsyncCallback.java.

--- src/java/nanomsg/async/IAsyncCallback.java
+++ src/java/nanomsg/async/IAsyncCallback.java
@@ -3,6 +3,6 @@ package nanomsg.async;
 import java.lang.Throwable;
 
 public interface IAsyncCallback<T> {
-    public void success(T result);
-    public void fail(Throwable t);
+  public void success(T result);
+  public void fail(Throwable t);
 }
