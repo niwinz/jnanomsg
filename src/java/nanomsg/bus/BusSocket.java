557175d9854ad5334e1ba118fbeaf5a47d10769e
Andrey Antukh <niwi@niwi.be>
2015-02-11 20:28:31 +0100

    Fix code style on BusSocket.java.

--- src/java/nanomsg/bus/BusSocket.java
+++ src/java/nanomsg/bus/BusSocket.java
@@ -5,11 +5,11 @@ import nanomsg.Nanomsg;
 
 
 public class BusSocket extends Socket {
-    public BusSocket(int domain) {
-        super(domain, Nanomsg.constants.NN_BUS);
-    }
+  public BusSocket(int domain) {
+    super(domain, Nanomsg.constants.NN_BUS);
+  }
 
-    public BusSocket() {
-        this(Nanomsg.constants.AF_SP);
-    }
+  public BusSocket() {
+    this(Nanomsg.constants.AF_SP);
+  }
 }
