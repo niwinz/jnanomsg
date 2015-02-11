4c20f348ab0ccaa4892c8dd0a12ae1294a769088
Andrey Antukh <niwi@niwi.be>
2015-02-11 20:29:36 +0100

    Fix code style on PubSocket.java.

--- src/java/nanomsg/pubsub/PubSocket.java
+++ src/java/nanomsg/pubsub/PubSocket.java
@@ -4,11 +4,11 @@ import nanomsg.Socket;
 import nanomsg.Nanomsg;
 
 public class PubSocket extends Socket {
-    public PubSocket(int domain) {
-        super(domain, Nanomsg.constants.NN_PUB);
-    }
+  public PubSocket(int domain) {
+    super(domain, Nanomsg.constants.NN_PUB);
+  }
 
-    public PubSocket() {
-        this(Nanomsg.constants.AF_SP);
-    }
+  public PubSocket() {
+    this(Nanomsg.constants.AF_SP);
+  }
 }
