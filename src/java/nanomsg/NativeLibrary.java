9b1847d942add6c211c2257c89de381295d579a0
Andrey Antukh <niwi@niwi.be>
2015-02-11 20:26:52 +0100

    Fix code style on NativeLibrary.java.

--- src/java/nanomsg/NativeLibrary.java
+++ src/java/nanomsg/NativeLibrary.java
@@ -7,23 +7,23 @@ import com.sun.jna.ptr.IntByReference;
 
 
 public class NativeLibrary {
-    public static native int nn_socket (int domain, int protocol);
-    public static native int nn_close (int s);
-    public static native int nn_bind (int s, String addr);
-    public static native int nn_connect (int s, String addr);
-    public static native int nn_send (int s, byte[] buff, int len, int flags);
-    public static native int nn_recv (int s, PointerByReference buff, int len, int flags);
-    public static native int nn_setsockopt (int s, int level, int option, Pointer optval, int optvallen);
-    public static native int nn_getsockopt (int s, int level, int option, Pointer optval, Pointer optvallen);
-    public static native int nn_freemsg (Pointer msg);
-    public static native int nn_errno ();
-    public static native int nn_device (int s1, int s2);
-    public static native void nn_term();
+  public static native int nn_socket (int domain, int protocol);
+  public static native int nn_close (int s);
+  public static native int nn_bind (int s, String addr);
+  public static native int nn_connect (int s, String addr);
+  public static native int nn_send (int s, byte[] buff, int len, int flags);
+  public static native int nn_recv (int s, PointerByReference buff, int len, int flags);
+  public static native int nn_setsockopt (int s, int level, int option, Pointer optval, int optvallen);
+  public static native int nn_getsockopt (int s, int level, int option, Pointer optval, Pointer optvallen);
+  public static native int nn_freemsg (Pointer msg);
+  public static native int nn_errno ();
+  public static native int nn_device (int s1, int s2);
+  public static native void nn_term();
 
-    public static native String nn_strerror (int errnum);
-    public static native Pointer nn_symbol (int i, IntByReference value);
+  public static native String nn_strerror (int errnum);
+  public static native Pointer nn_symbol (int i, IntByReference value);
 
-    static {
-        Native.register("nanomsg");
-    }
+  static {
+    Native.register("nanomsg");
+  }
 }
