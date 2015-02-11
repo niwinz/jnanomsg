3888d3412daff839b9077405a4b33ba90ffff1ad
Andrey Antukh <niwi@niwi.be>
2015-02-11 20:28:18 +0100

    Fix code style on SimpleAsyncScheduler.java.

--- src/java/nanomsg/async/SimpleAsyncScheduler.java
+++ src/java/nanomsg/async/SimpleAsyncScheduler.java
@@ -16,52 +16,52 @@ import nanomsg.exceptions.IOException;
 
 
 public class SimpleAsyncScheduler implements Runnable, IAsyncScheduler {
-    private final LinkedBlockingQueue<IAsyncRunnable> queue = new LinkedBlockingQueue<IAsyncRunnable>();
-    private final int concurrency = Runtime.getRuntime().availableProcessors();
-    private final AtomicBoolean started = new AtomicBoolean(false);
+  private final LinkedBlockingQueue<IAsyncRunnable> queue = new LinkedBlockingQueue<IAsyncRunnable>();
+  private final int concurrency = Runtime.getRuntime().availableProcessors();
+  private final AtomicBoolean started = new AtomicBoolean(false);
 
-    public static final IAsyncScheduler instance = new SimpleAsyncScheduler();
+  public static final IAsyncScheduler instance = new SimpleAsyncScheduler();
 
-    private void startThreadGroup() {
-        final ThreadGroup group = new ThreadGroup("nanomsg-scheduler");
-        group.setDaemon(false);
+  private void startThreadGroup() {
+    final ThreadGroup group = new ThreadGroup("nanomsg-scheduler");
+    group.setDaemon(false);
 
-        for (int i=0; i<concurrency; ++i) {
-            final Thread t = new Thread(group, this);
-            t.setDaemon(false);
-            t.start();
+    for (int i=0; i<concurrency; ++i) {
+      final Thread t = new Thread(group, this);
+      t.setDaemon(false);
+      t.start();
+    }
+  }
+
+  public void schedule(final IAsyncRunnable handler) throws InterruptedException {
+    /* Start scheduler if it not started */
+    if (started.compareAndSet(false, true)) {
+      startThreadGroup();
+    }
+
+    queue.put(handler);
+  }
+
+  @Override
+  public String toString() {
+    return "pending=" + queue.size() + ", pool started:" + started.get();
+  }
+
+  public void run() {
+    while (true) {
+      try {
+        final IAsyncRunnable handler = queue.take();
+        try {
+          handler.run();
+        } catch (IOException e) {
+          final int errno = e.getErrno();
+          if (errno == Nanomsg.constants.EAGAIN) {
+            queue.put(handler);
+          }
         }
+      } catch (InterruptedException e) {
+        break;
+      }
     }
-
-    public void schedule(final IAsyncRunnable handler) throws InterruptedException {
-        /* Start scheduler if it not started */
-        if (started.compareAndSet(false, true)) {
-            startThreadGroup();
-        }
-
-        queue.put(handler);
-    }
-
-    @Override
-    public String toString() {
-        return "pending=" + queue.size() + ", pool started:" + started.get();
-    }
-
-    public void run() {
-        while (true) {
-            try {
-                final IAsyncRunnable handler = queue.take();
-                try {
-                    handler.run();
-                } catch (IOException e) {
-                    final int errno = e.getErrno();
-                    if (errno == Nanomsg.constants.EAGAIN) {
-                        queue.put(handler);
-                    }
-                }
-            } catch (InterruptedException e) {
-                break;
-            }
-        }
-    }
+  }
 }
