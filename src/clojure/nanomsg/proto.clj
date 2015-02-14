a2d01030ebcfd85c7a6ed4b5be8462ca4f189823
Andrey Antukh <niwi@niwi.be>
2015-02-14 20:24:52 +0100

    Split a protocol to sockets and data into separate ns.

---
+++ src/clojure/nanomsg/proto.clj
@@ -0,0 +1,23 @@
+(ns nanomsg.proto
+  "Protocol definitions for nanomsg sockets."
+  (:refer-clojure :exclude [send]))
+
+(defprotocol ISocket
+  "Common socket protocol."
+  (bind [_ dir])
+  (connect [_ dir])
+  (subscribe [_ pattern])
+  (unsubscribe [_ pattern])
+  (send [_ data opt])
+  (recv [_ opt]))
+
+(defprotocol IBlockingSocket
+  "Mark for blocking socket.")
+
+(defprotocol IAsyncSocket
+  "Mark for async socket.")
+
+(defprotocol ISocketData
+  "Common interface for data that can be
+  sended through nanomsg socket."
+  (get-bytes [_] "Get a byte array representation."))
