44d1716006e2abb01501569ea9f1393292f49d48
Andrey Antukh <niwi@niwi.be>
2015-02-15 13:46:57 +0100

    Initial work on new tests.

--- test/nanomsg_tests.clj
+++ test/nanomsg_tests.clj
@@ -1,6 +1,6 @@
 (ns nanomsg-tests
   (:require [nanomsg.core :as nn]
-            [nanomsg.async :as nna]
+            ;; [nanomsg.async :as nna]
             [clojure.core.async :refer [<! >! go put! take!]]
             [clojure.test :refer :all]))
 
@@ -32,95 +32,102 @@
 ;;   (testing "FooBar"
 ;;     (let [p (promise)
 ;;           r1 (future
-;;                (with-open [sock (nn/socket :rep {:async true})]
+;;                (let [sock (nn/socket :rep {:async true})]
+;;                  (println 111)
 ;;                  (nn/bind! sock s-bind-tcp)
-;;                  (nn/recv! sock (fn [data error]
-;;                                   (println "Received data:" data error)
-;;                                   (nn/send! sock "pong")))))
+;;                  (nn/recv-str! sock (fn [data error]
+;;                                   (println "Server: received data " data error)
+;;                                   (nn/send! sock "pong"
+;;                                             (fn [& args]
+;;                                               (println "Server: echoed data")
+;;                                               (nn/close! sock)))))))
 ;;           r2 (future
-;;                (with-open [sock (nn/socket :req {:async true})]
+;;                (let [sock (nn/socket :req {:async true})]
+;;                  (println 222)
 ;;                  (nn/connect! sock s-bind-tcp)
-;;                  (nn/send! sock "foobar")
+;;                  (nn/send! sock "foobar" (fn [& args]
+;;                                            (println "Client: sended data " args)))
 ;;                  (nn/recv! sock (fn [data error]
-;;                                   (deliver p [data error])))))]
+;;                                   (println "Client: received data " data error)
+;;                                   (deliver p [data error])
+;;                                   (nn/close! sock)))))]
 ;;       (println @p)
 ;;       (is (= 1 2)))))
 
-(deftest socket-req-rep
+
+(deftest sync-sockets
   (testing "ReqRep"
-    (with-open [req-sock (nn/socket :req)]
-      (let [result (future
-                     (with-open [rep-sock (nn/socket :rep)]
-                       (nn/bind! rep-sock s-bind-ipc)
-                       (let [received (nn/recv-str! rep-sock)]
-                         (nn/send! rep-sock received)
-                         received)))]
-        (sleep 500)
-        (nn/connect! req-sock s-bind-ipc)
-        (nn/send! req-sock "foo")
-        (is (= (nn/recv-str! req-sock) "foo"))))))
+    (let [p (promise)]
+      (future
+        (with-open [sock (nn/socket :rep)]
+          (nn/bind! sock s-bind-tcp)
+          (let [received (nn/recv! sock)]
+            (nn/send! sock received))))
+      (future
+       (with-open [sock (nn/socket :req)]
+          (nn/connect! sock s-bind-tcp)
+          (nn/send! sock "foo")
+          (deliver p (nn/recv! sock))))
 
-(deftest socket-pub-sub
-  (testing "PubSub"
-    (with-open [pub-sock (nn/socket :pub)]
-      (nn/bind! pub-sock s-bind-ipc)
-      (let [recvd1 (future
-                     (with-open [sub-socket (nn/socket :sub)]
-                       (nn/connect! sub-socket s-bind-ipc)
-                       (nn/subscribe! sub-socket "ss1")
-                       (nn/recv-str! sub-socket)))
-            recvd2 (future
-                     (with-open [sub-socket (nn/socket :sub)]
-                       (nn/connect! sub-socket s-bind-ipc)
-                       (nn/subscribe! sub-socket "ss2")
-                       (nn/recv-str! sub-socket)))]
+      (println "Result:" @p)))
 
-        (sleep 500)
-        (nn/send! pub-sock "ss2 foo")
-        (nn/send! pub-sock "ss1 bar")
+  ;; (testing "PubSub"
+  ;;   (with-open [pub-sock (nn/socket :pub)]
+  ;;     (nn/bind! pub-sock s-bind-ipc)
+  ;;     (let [recvd1 (future
+  ;;                    (with-open [sub-socket (nn/socket :sub)]
+  ;;                      (nn/connect! sub-socket s-bind-ipc)
+  ;;                      (nn/subscribe! sub-socket "ss1")
+  ;;                      (nn/recv-str! sub-socket)))
+  ;;           recvd2 (future
+  ;;                    (with-open [sub-socket (nn/socket :sub)]
+  ;;                      (nn/connect! sub-socket s-bind-ipc)
+  ;;                      (nn/subscribe! sub-socket "ss2")
+  ;;                      (nn/recv-str! sub-socket)))]
 
-        (is (= @recvd1 "ss1 bar"))
-        (is (= @recvd2 "ss2 foo"))))))
+  ;;       (sleep 500)
+  ;;       (nn/send! pub-sock "ss2 foo")
+  ;;       (nn/send! pub-sock "ss1 bar")
 
-(deftest socket-pipeline
-  (testing "Pipeline"
-    (let [sockname  (str s-bind-ipc "pipeline")]
-      (with-open [push-sock (nn/socket :push {:bind sockname})]
-        (let [receiver  (future
-                          (with-open [pull-sock (nn/socket :pull {:connect sockname})]
-                            (let [received  (nn/recv-str! pull-sock)]
-                              (nn/close! pull-sock)
-                              received)))]
-          (sleep 500)
-          (nn/send! push-sock "message1")
-          (is (= @receiver "message1")))))))
+  ;;       (is (= @recvd1 "ss1 bar"))
+  ;;       (is (= @recvd2 "ss2 foo")))))
 
+  ;; (testing "Pipeline"
+  ;;   (let [sockname  (str s-bind-ipc "pipeline")]
+  ;;     (with-open [push-sock (nn/socket :push {:bind sockname})]
+  ;;       (let [receiver  (future
+  ;;                         (with-open [pull-sock (nn/socket :pull {:connect sockname})]
+  ;;                           (let [received  (nn/recv-str! pull-sock)]
+  ;;                             (nn/close! pull-sock)
+  ;;                             received)))]
+  ;;         (sleep 500)
+  ;;         (nn/send! push-sock "message1")
+  ;;         (is (= @receiver "message1"))))))
 
-(deftest socket-pipeline
-  (testing "Pair"
-    (with-open [sock (nn/socket :pair)]
-      (nn/bind! sock s-bind-tcp)
-      (let [recvd1 (future
-                     (with-open [socket (nn/socket :pair)]
-                       (nn/connect! socket s-bind-tcp)
-                       (nn/recv-str! socket)))]
-        (sleep 500)
-        (nn/send! sock "foo")
-        (is (= @recvd1 "foo"))))))
+  ;; (testing "Pair"
+  ;;   (with-open [sock (nn/socket :pair)]
+  ;;     (nn/bind! sock s-bind-tcp)
+  ;;     (let [recvd1 (future
+  ;;                    (with-open [socket (nn/socket :pair)]
+  ;;                      (nn/connect! socket s-bind-tcp)
+  ;;                      (nn/recv-str! socket)))]
+  ;;       (sleep 500)
+  ;;       (nn/send! sock "foo")
+  ;;       (is (= @recvd1 "foo")))))
 
-(deftest socket-pipeline
-  (testing "Bus"
-    (with-open [sock (nn/socket :bus)]
-      (nn/bind! sock s-bind-ipc)
-      (let [recvd1   (future
-                       (with-open [socket (nn/socket :bus)]
-                         (nn/connect! socket s-bind-ipc)
-                         (nn/recv-str! socket)))
-            recvd2   (future
-                       (with-open [socket (nn/socket :bus)]
-                         (nn/connect! socket s-bind-ipc)
-                         (nn/recv-str! socket)))]
-        (sleep 500)
-        (nn/send! sock "foo")
-        (is (= @recvd1 "foo"))
-        (is (= @recvd2 "foo"))))))
+  ;; (testing "Bus"
+  ;;   (with-open [sock (nn/socket :bus)]
+  ;;     (nn/bind! sock s-bind-ipc)
+  ;;     (let [recvd1   (future
+  ;;                      (with-open [socket (nn/socket :bus)]
+  ;;                        (nn/connect! socket s-bind-ipc)
+  ;;                        (nn/recv-str! socket)))
+  ;;           recvd2   (future
+  ;;                      (with-open [socket (nn/socket :bus)]
+  ;;                        (nn/connect! socket s-bind-ipc)
+  ;;                        (nn/recv-str! socket)))]
+  ;;       (sleep 500)
+  ;;       (nn/send! sock "foo")
+  ;;       (is (= @recvd1 "foo"))
+  ;;       (is (= @recvd2 "foo")))))
+)
