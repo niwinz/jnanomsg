f43bab7f0f0abb4c488f3f14203ac82c6a95bfe2
Andrey Antukh <niwi@niwi.be>
2015-03-17 20:06:57 +0100

    Add octet to dependencies list.

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.apache.org/licenses/LICENSE-2.0.txt"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [funcool/octet "0.1.0"]
                  [net.java.dev.jna/jna "4.1.0"]]
   :jvm-opts ["-server" "-Xmx2g" "-XX:+UseG1GC"]
   :javac-options ["-target" "1.7" "-source" "1.7" "-Xlint:-options"]
