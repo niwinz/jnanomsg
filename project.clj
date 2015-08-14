5c97b943e2e1b9b3d3de5002da3c6066969faacf
Andrey Antukh <niwi@niwi.nz>
2015-08-14 07:14:45 +0300

    Update changelog and set version to 0.4.1.

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject jnanomsg "0.4.0"
+(defproject jnanomsg "0.4.1"
   :description "Nanomsg library for Java & Clojure"
   :url "https://github.com/niwibe/jnanomsg"
   :license {:name "Apache 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.txt"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0" :scope "provided"]
                  [funcool/octet "0.1.0"]
                  [net.java.dev.jna/jna "4.1.0"]]
 
@@ -16,12 +16,13 @@
   :javadoc-opts {:output-dir "doc/api/java"
                  :package-names ["nanomsg" "nanomsg.jna" "nanomsg.pubsub" "nanomsg.exceptions"
                                  "nanomsg.reqrep" "nanomsg.bus" "nanomsg.pair" "nanomsg.pipeline"]}
-  :codeina {:output-dir "doc/api/clojure"
-            :src-dir-uri "http://github.com/niwibe/jnanomsg/blob/master/"
-            :src-linenum-anchor-prefix "L"
+
+  :codeina {:target "doc/api/clojure"
+            :src-uri "http://github.com/niwibe/jnanomsg/blob/master/"
+            :src-uri-prefix "L"
             :exclude [nanomsg.benchmark nanomsg.impl]}
 
   :profiles {:dev {:plugins [[lein-javadoc "0.1.1"]
-                             [funcool/codeina "0.1.0-SNAPSHOT"]]
+                             [funcool/codeina "0.2.0"]]
                    :main nanomsg.benchmark}
              :uberjar {:aot :all}})
